33e8ed89f5058c86a8b56ce5ff28649977b48511
Answed <schradearwed@gmail.com>
2023-04-11 15:11:47 +0200

    Added Movie List

---
+++ Simple API/Simple API/Controllers/MovieListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_API.Controllers
+{
+    public class MovieListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
