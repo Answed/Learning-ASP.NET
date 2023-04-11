49f3fe106f97a93c2aad7b271d7e9784c013034f
Answed <schradearwed@gmail.com>
2023-04-11 15:06:44 +0200

    Added Views
    Learned how Controllers and Views are connected with each other and how i can use one to generate html pages for me

--- Simple API/Simple API/Controllers/HelloWorldController.cs
+++ Simple API/Simple API/Controllers/HelloWorldController.cs
@@ -4,18 +4,19 @@ namespace Simple_API.Controllers
 {
     public class HelloWorldController : Controller
     {
-
         //
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
         //
         // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1) 
         {
-            return $"Hello {name}, NumTime is: {numTimes}";
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
