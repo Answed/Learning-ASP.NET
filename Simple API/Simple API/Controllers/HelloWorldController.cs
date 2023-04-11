837a639ed199fe215a3c76447bfe9f23605f907b
Answed <schradearwed@gmail.com>
2023-04-11 12:46:48 +0200

    Created HelloWorld Controller
    Created a controller and played around with URL Routing

---
+++ Simple API/Simple API/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_API.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return $"Hello {name}, NumTime is: {numTimes}";
+        }
+    }
+}
