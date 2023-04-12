ccdb7ae5be37895555d95cab07faf27be28407ee
Answed <schradearwed@gmail.com>
2023-04-12 15:00:54 +0200

    Added datamodell class

---
+++ Simple API/Simple API/Data/MovieListContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Simple_API.Models;
+
+namespace Simple_API.Data
+{
+    public class MovieListContext : DbContext
+    {
+        public MovieListContext (DbContextOptions<MovieListContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Simple_API.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Simple API/Simple API/Program.cs
+++ Simple API/Simple API/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Simple_API.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieListContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieListContext") ?? throw new InvalidOperationException("Connection string 'MovieListContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
