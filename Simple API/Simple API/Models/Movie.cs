5b5832daf55eace063b671c529b6683858c52406
Answed <schradearwed@gmail.com>
2023-04-12 17:05:36 +0200

    Changed the Title

--- Simple API/Simple API/Models/Movie.cs
+++ Simple API/Simple API/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Simple_API.Models
 {
@@ -6,9 +7,11 @@ namespace Simple_API.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        [DataType(DataType.Date)] 
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }  
 }
